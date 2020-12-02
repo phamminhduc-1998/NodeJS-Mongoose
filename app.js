cfdcb3a8ce861277a67e81adbba1c507841cb588
phamminhduc-1998 <56750938+phamminhduc-1998@users.noreply.github.com>
2020-12-02 17:20:48 +0700

    Mongoose

--- app.js
+++ app.js
@@ -11,17 +11,32 @@ mongoose.connect('mongodb+srv://Phamminhduc98:1998@cluster0.mbuwn.mongodb.net/Te
 let adminSchema = require('./model/adminSchema'); //lay data tu dung link
 let Admin = mongoose.model('Admin1998', adminSchema);//(ten bang, schema)
 
-var Admin1 = Admin({
-    name: "duc1",
-    pass: "1",
-});
+// var Admin1 = Admin({
+//     name: "duc1998",
+//     pass: "1",
+// });
 
-Admin1.save(function(err){
-    if(err) throw err;
-    console.log("OK SAVE");
-})
+// Admin1.save(function(err){
+//     if(err) throw err;
+//     console.log("OK SAVE");
+// })
+
+// //find all data
+// Admin.find({}, function (err, docs) {
+//     console.log(docs);
+// });
 
 
+//deleter theo id
+// Admin.findById("5fc76101152ee11258852028", function (err, post) {
+//     if (!err) {
+//         post.remove();
+//         post.save(function (err) {
+//             // do something
+//         });
+//         console.log(post);
+//     }
+// });
 
 
 
